9c25f8a2e86bb3d48136a1ea0555d0782d8e474e
jmacpherson <justin.macpherson@scs.ch>
2020-06-05 16:56:19 +0200

    added beginnings of an LRP kernel

--- lrp.cu
+++ lrp.cu
@@ -5,71 +5,100 @@ using namespace std;
 
 
 // function to convolve the elements of two arrays
-__global__ void conv(float *out, float *in, float *kernel, int width, int height, int infeat, int outfeat) {
+__global__ void conv_relu(float *out, float *in, float *kernel, int width, int height, int infeat, int outfeat) {
 	int outf = blockIdx.x;
 	int x = threadIdx.x;
 	int y = threadIdx.y;
 	int outIdx = x + 1 + (y + 1) * width + outf * width * height;
-	for (int inf = 0; inf < infeat; inf++) {
+	for (int inf = 0; inf < infeat; inf++){	
 		for (int u = 0; u < 3; u++){
 			for (int v = 0; v < 3; v++) {
-				int inIdx = (x + u) + (y + v) * width + inf * width * height;
-				out[outIdx] += in[inIdx] * kernel[u + v * 3 + inf * 9 + outf * 9 * infeat];
+				out[outIdx] += in[(x + u) + (y + v) * width + inf * width * height] * kernel[u + v * 3 + inf * 9 + outf * 9 * infeat];
 			}
 		}
-	}		
+	}
+	if (out[outIdx] < 0) {out[outIdx] = 0.0f; }	
+}
+
+
+__global__ void lrp(float *relevance, float *out, float *kernel, int width, int height, int infeat, int outfeat) {
+	int inf = blockIdx.x;
+	int x = threadIdx.x;
+	int y = threadIdx.y;
+	float activations[25 * infeat * outfeat];
+	for (int inf = 0; inf < infeat; inf++) {
+		for (int u = 0; u < 5; u++) {
+			for (int v = 0; v < 5; v++) {
+				activations[u + v * 5 + inf * 25 + outf * infeat * 25] = in[(x + u) + (y + v) * width + inf * width * height] * kernel[u + v * 3 + inf * 9 + outf * 9 * infeat];
+			}	
+		}		
+	}
 }
 
 
 int main(void) {
-	int N = 4;
-	int M = 4;
-	int infeat = 2;
-	int outfeat = 2;
-	int kernelh = 3;
-	int kernelw = 3;
-
-	float *x, *y, *kernel;
 	
-	// initialize x and y arrays on the host
-	// Allocate Unified Memory – accessible from CPU or GPU
-	cudaMallocManaged(&x, N * M * infeat * sizeof(float));
-	cudaMallocManaged(&y, N * M * outfeat * sizeof(float));
-	cudaMallocManaged(&kernel, kernelh * kernelw * sizeof(float));
+	//Define arrays
+	float *input, *output, *kernel, *activations;
+	cudaError_t s;
 
-	for (int i = 0; i < infeat * N * M; i++) {
-		x[i] = 1.0f;
+	//Define sizes
+	int imgHeight = 30;
+	int imgWidth = 30;
+	int kernelHeight = 3;
+	int kernelWidth = 3;
+	int inFeat = 2;
+	int outFeat = 2;
+	int nInput = imgHeight * imgWidth * inFeat;
+	int nOutput = imgHeight * imgWidth * outFeat;
+	int nWeights = kernelHeight * kernelWidth * inFeat * outFeat;
+	int nActivations = (kernelHeight * 2 - 1) * (kernelWidth * 2 - 1) * inFeat * outFeat;
+	float output_[nOutput];
+	float input_[nInput];
+	float kernel_[nWeights];
+	float activations_[nActivations]
+	
+	//Initialize inputs
+	for (int i = 0; i < nInput; i++) {
+		input_[i] = 1.0f;
 	}
-
-	for (int i = 0; i < outfeat * N * M; i++) {
-		y[i] = 0.0f;
+	for (int i = 0; i < nWeights; i++) {
+		kernel_[i] = 1.0f;
 	}
-
-	for (int i = 0; i < infeat * outfeat * kernelw * kernelh; i++) {
-		kernel[i] = 1.0f;
-	}
-
+	
+	//Perform memory operations
+	cudaMalloc((void **) &input, nInput * sizeof(float));
+	cudaMalloc((void **) &output, nOutput * sizeof(float));
+	cudaMalloc((void **) &kernel, nWeights * sizeof(float));
+	cudaMalloc((void **) &activations,l nActivations * sizeof(float));
+	cudaMemset((void **) output, 0, nOutput * sizeof(float));
+	cudaMemset((void **) activations, 0, nActivations * sizeof(float));
+	cudaMemcpy(input, input_, nInput * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(kernel, kernel_, nWeights * sizeof(float), cudaMemcpyHostToDevice);
 
 	// Run kernel on 1M elements on the CPU
-	conv <<<outfeat, dim3(N - 2, M - 2)>>> (y, x, kernel, N, M, infeat, outfeat);
+	conv_relu <<<outFeat, dim3(imgWidth - 2, imgHeight - 2)>>> (output, input, kernel, imgWidth, imgHeight, inFeat, outFeat);
 
 	cudaDeviceSynchronize();
 
+	s = cudaMemcpy(output_, output, nOutput * sizeof(float), cudaMemcpyDeviceToHost);
+	cout << cudaGetErrorName(s) << endl;
+
 	// Check for errors (all values should be 3.0f)
 	float maxError = 0.0f;
-	for (int i = 1; i < N - 1; i++) {
-		for (int j = 1; j < M - 1; j++) {
-			for (int o = 0; o < outfeat; o++) {
-				maxError = fmax(maxError, fabs(y[i + j * N + o * N * M] - 18.0f));
+	for (int i = 1; i < imgWidth - 1; i++) {
+		for (int j = 1; j < imgHeight - 1; j++) {
+			for (int o = 0; o < outFeat; o++) {
+				maxError = fmax(maxError, fabs(output_[i + j * imgWidth + o * imgHeight * imgWidth] - 18.0f));
 			}
 		}
 	}
 	cout << "Max error: " << maxError << endl;
 
-	for (int k = 0; k < outfeat; k ++) {
-		for (int j = 0; j < M; j++) {
-			for (int i = 0; i < N; i++) {
-				cout << y[i + j * N + k * N * M] << " ";
+	for (int k = 0; k < outFeat; k ++) {
+		for (int i = 0; i < imgWidth; i++) {
+			for (int j = 0; j < imgHeight; j++) {
+				cout << output_[i + j * imgWidth + k * imgWidth * imgHeight] << " ";
 			}
 			cout << endl;
 		}
@@ -77,9 +106,8 @@ int main(void) {
 	}
 
 	// Free memory
-	cudaFree(x);
-	cudaFree(y);
+	cudaFree(input);
+	cudaFree(output);
 	cudaFree(kernel);
-
 	return 0;
 }
